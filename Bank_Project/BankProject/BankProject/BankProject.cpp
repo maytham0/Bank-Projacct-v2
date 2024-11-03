712f7186bb338271c283a9b13bf1b6d08abb67f6
Maytham Hassn <124174844+maytham0@users.noreply.github.com>
2024-11-03 15:13:54 +0300

    Add files via upload

---
+++ Bank_Project/BankProject/BankProject/BankProject.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cctype>
+#include "FuncionToBankProject.h"
+#include<iomanip>
+#include"ShowFunion.h"
+
+using namespace std;
+using namespace Screen;
+using namespace BankFuncion;
+int main()
+{
+		ElsePage();
+
+	return 0;
+}
